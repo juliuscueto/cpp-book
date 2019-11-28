9e83990955cf22dc5cbb7e9641c539676ff14ece
juliuscueto <juliuscueto@gmail.com>
2019-11-28 18:03:06 +0900

    [mod] finished to chapter007

--- main.cpp
+++ main.cpp
@@ -16,26 +16,8 @@ std::string plus(std::string x, std::string y){
 
 int main(int argc, char const *argv[])
 {
-    auto cat = "cat"s ;
-    auto dog = "dog"s ;
-
-    if ( cat < dog )
-    {   // 猫は小さい
-        std::cout << "cat is smaller.\n"s ;
-    }
-    else
-    {   // 犬は小さい
-        std::cout << "dog is smaller.\n"s ;
-    }
-
-    auto longcat = "longcat"s ;
-
-    if ( longcat > cat )
-    {   // longcatは長い
-        std::cout << "Longcat is Looong.\n"s ;
-    }
-    else
-    {
-        std::cout << "Longcat isn't that long. Sigh.\n"s ;
-    }
+    std::string x{};
+    std::string y{};
+    std::cin >> x >> y;
+    std::cout << x << y;
 }
