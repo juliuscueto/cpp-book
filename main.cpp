9d1dbb4f3a53629f8e7b64b42b6cc9ce4ff88e4d
juliuscueto-surface <juliuscueto@gmail.com>
2019-11-29 11:40:57 +0900

    [mod] finished to chapter014

--- main.cpp
+++ main.cpp
@@ -1,23 +1,19 @@
 #include "all.h"
 
-int plus(int x, int y)
-{
-    return x + y;
-}
-
-double plus(double x, double y)
-{
-    return x + y;
-}
-
-std::string plus(std::string x, std::string y){
-    return x + y;
-}
+auto output_all = [](auto first, auto last, auto output_iter){
+    for (auto iter = first; iter != last; iter++, output_iter++)
+    {
+        *output_iter = *iter;
+    }
+};
 
 int main(int argc, char const *argv[])
 {
-    std::string x{};
-    std::string y{};
-    std::cin >> x >> y;
-    std::cout << x << y;
+    std::vector<int> v = {1, 2, 3, 4, 5,};
+    std::vector<int> destination{};
+    output_all(
+        std::begin(v),
+        std::end(v),
+        std::begin(destination)
+    );  
 }
