73435f94e2869ba93b0f557de624a3e9b431068d
juliuscueto <juliuscueto@gmail.com>
2019-12-03 19:23:52 +0900

    [mod] chapter029 midway: finished to pointers of objects

--- main.cpp
+++ main.cpp
@@ -14,6 +14,31 @@ struct array_iterator
         return a[i];
     }
 
+    typename Array::reference operator [] (std::size_t n) const
+    {
+        return *(*this + n);
+    }
+
+    bool operator < (array_iterator const & right) const
+    {
+        return i < right.i;
+    }
+
+    bool operator <= (array_iterator const & right) const
+    {
+        return i <= right.i;
+    }
+
+    bool operator > (array_iterator const & right) const
+    {
+        return i > right.i;
+    }
+
+    bool operator >= (array_iterator const & right) const
+    {
+        return i >= right.i;
+    }
+
     array_iterator & operator ++()
     {
         ++i;
@@ -24,6 +49,19 @@ struct array_iterator
     {
         array_iterator copy = *this;
         ++*this;
+        return copy;
+    }
+
+    array_iterator operator +=(std::size_t n)
+    {
+        i += n;
+        return *this;
+    }
+
+    array_iterator operator + (std::size_t n) const
+    {
+        auto copy = *this;
+        copy += n;
         return copy;
     }
 
@@ -39,6 +77,114 @@ struct array_iterator
         ++*this;
         return copy;
     }
+
+    array_iterator operator -=(std::size_t n)
+    {
+        i -= n;
+        return *this;
+    }
+
+    array_iterator operator - (std::size_t n) const
+    {
+        auto copy = *this;
+        copy -= n;
+        return copy;
+    }
+};
+
+template<typename Array>
+struct array_const_iterator
+{
+    Array & a;
+    std::size_t i;
+
+    array_const_iterator(typename array_iterator<Array>::iterator const & iter)
+        : a(iter.a), i(iter.i){}
+
+    array_const_iterator(Array const & a, std::size_t i)
+        : a(a), i(i){}
+
+    typename Array::const_reference operator*() const
+    {
+        return a[i];
+    }
+
+    typename Array::const_reference operator [] (std::size_t n) const
+    {
+        return *(*this + n);
+    }
+
+    bool operator < (array_const_iterator const & right) const
+    {
+        return i < right.i;
+    }
+
+    bool operator <= (array_const_iterator const & right) const
+    {
+        return i <= right.i;
+    }
+
+    bool operator > (array_const_iterator const & right) const
+    {
+        return i > right.i;
+    }
+
+    bool operator >= (array_const_iterator const & right) const
+    {
+        return i >= right.i;
+    }
+
+    array_const_iterator & operator ++()
+    {
+        ++i;
+        return *this;
+    }
+
+    array_const_iterator operator ++(int)
+    {
+        array_const_iterator copy = *this;
+        ++*this;
+        return copy;
+    }
+
+    array_const_iterator operator +=(std::size_t n)
+    {
+        i += n;
+        return *this;
+    }
+
+    array_const_iterator operator + (std::size_t n) const
+    {
+        auto copy = *this;
+        copy += n;
+        return copy;
+    }
+
+    array_const_iterator & operator --()
+    {
+        --i;
+        return *this;
+    }
+
+    array_const_iterator operator --(int)
+    {
+        array_const_iterator copy = *this;
+        ++*this;
+        return copy;
+    }
+
+    array_const_iterator operator -=(std::size_t n)
+    {
+        i -= n;
+        return *this;
+    }
+
+    array_const_iterator operator - (std::size_t n) const
+    {
+        auto copy = *this;
+        copy -= n;
+        return copy;
+    }
 };
 
 template <typename T, std::size_t N>
@@ -50,6 +196,7 @@ struct array
 
     using size_type = std::size_t;
     using iterator = array_iterator<array>;
+    using const_iterator = array_const_iterator<array>;
 
     value_type storage[N];
 
@@ -63,10 +210,22 @@ struct array
         return storage[i];
     }
 
+    reference at(std::size_t n)
+    {
+        if ( n >= size()) throw std::out_of_range("Error: Out of Range");
+        return storage[n];
+    }
+
+    const_reference at(std::size_t n) const
+    {
+        if ( n >= size()) throw std::out_of_range("Error: Out of Range");
+        return storage[n];
+    }
+
     size_type size() const
     {
         return sizeof(storage)/sizeof(value_type);
-    };
+    }
 
     reference front(){return storage[0];}
     const_reference front() const{return storage[0];}
@@ -80,6 +239,21 @@ struct array
     iterator end()
     {
         return iterator(*this, N);
+    }
+
+    const_iterator begin() const {
+        return const_iterator(*this, 0);
+    }
+    const_iterator end() const
+    {
+        return const_iterator(*this, N);
+    }
+    const_iterator cbegin() const {
+        return const_iterator(*this, 0);
+    }
+    const_iterator cend() const
+    {
+        return const_iterator(*this, N);
     }
 };
 
@@ -95,13 +269,8 @@ void print(Array const & c)
 
 int main(int argc, char const *argv[])
 {
-    array<int, 5> a = {1,2,3,4,5};
-    auto iter = a.begin();
-    std::cout << *iter << std::endl;
-    ++iter;
-    std::cout << *iter << std::endl;
-    ++iter;
-    std::cout << *iter << std::endl;
-    auto last = a.end();
-    std::cout << *last << std::endl;
+    std::add_pointer_t<int> a = nullptr;
+    std::add_pointer_t<int *> b = nullptr;
+    std::add_pointer_t<int(int)> c = nullptr;
+
 }
