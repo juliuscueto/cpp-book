218a458fd93f9ef92f04686b78508128f951f6b5
juliuscueto <juliuscueto@gmail.com>
2019-12-04 22:12:04 +0900

    [mod] finished to chapter031: finished iterators

--- main.cpp
+++ main.cpp
@@ -267,10 +267,143 @@ void print(Array const & c)
     
 }
 
+template<typename T>
+void print_size()
+{
+    std::cout << sizeof(T) << std::endl;
+}
+
+void * memcpy(void * dest, void const * src, std::size_t n)
+{
+    auto d = static_cast<std::byte *>(dest);
+    auto s = static_cast<std::byte const *>(src);
+
+    for (std::size_t i = 0; i < n; i++)
+    {    
+        d[i] = s[i];
+    }
+    return dest;
+}
+
+template<typename To, typename From>
+To bit_cast(From const & from)
+{
+    To to;
+    memcpy(&to, &from, sizeof(To));
+    return to;
+}
+
+template<typename T>
+struct iota_iterator
+{
+    using difference_type = std::ptrdiff_t;
+    using value_type = T;
+    using reference = T &;
+    using pointer = T *;
+    using iterator_category = std::bidirectional_iterator_tag;
+
+    T value;
+
+    iota_iterator(T value = 0)
+        : value(value) {}
+
+    iota_iterator & operator +=(difference_type n)
+    {
+        value += n;
+        return *this;
+    }
+    iota_iterator operator +(difference_type n) const
+    {
+        auto temp = *this;
+        temp += n;
+        return *temp;
+    }
+    iota_iterator operator -=(difference_type n)
+    {
+        value -= n;
+        return *this;
+    }
+    iota_iterator operator -(difference_type n) const
+    {
+        auto temp = *this;
+        temp -= n;
+        return *temp;
+    }
+    reference operator *() noexcept {return value;}
+    const reference operator *() const noexcept {return value;}
+
+    iota_iterator & operator ++() noexcept
+    {
+        ++value;
+        return *this;
+    }
+    iota_iterator operator ++(int) noexcept
+    {
+        auto temp = *this;
+        ++*this;
+        return temp;
+    }
+
+    iota_iterator & operator --() const noexcept
+    {
+        --value;
+        return *this;
+    }
+
+    iota_iterator operator --(int) const noexcept
+    {
+        auto temp = *this;
+        --*this;
+        return temp;
+    }
+
+    bool operator == (iota_iterator const & i) const noexcept
+    {
+        return value == i.value;
+    }
+    bool operator != (iota_iterator const & i) const noexcept
+    {
+        return !(*this == i);
+    }
+
+    difference_type operator - (iota_iterator const & i)
+    {
+        return value - i.value;
+    }
+    bool operator < (iota_iterator const & i) const noexcept
+    {return value < i.value;}
+    bool operator > (iota_iterator const & i) const noexcept
+    {return value > i.value;}
+    bool operator <= (iota_iterator const & i) const noexcept
+    {return value <= i.value;}
+    bool operator >= (iota_iterator const & i) const noexcept
+    {return value >= i.value;}
+};
+
+template<typename T>
+iota_iterator<T> operator +(
+    typename iota_iterator<T>::difference_type n,
+    iota_iterator<T> const & i
+)
+{return i + n;}
+
+template<typename T>
+iota_iterator<T> operator -(
+    typename iota_iterator<T>::difference_type n,
+    iota_iterator<T> const & i
+)
+{return i - n;}
+
 int main(int argc, char const *argv[])
 {
-    std::add_pointer_t<int> a = nullptr;
-    std::add_pointer_t<int *> b = nullptr;
-    std::add_pointer_t<int(int)> c = nullptr;
+    std::vector<int> v = {1,2,3,4,5};
+    iota_iterator<int> i;
+    iota_iterator first(0), last(10);
+    iota_iterator non_const(0);
+    int value = *non_const;
+    value = 1;
 
+    iota_iterator immutable(0);
+    int const const_value = *immutable;
+    const_value = 1;
 }
